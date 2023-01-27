fc6a2b436bf79eab320f814b8dca6209f1b12aa2
Khashayar Shomali <khashi792017@gmail.com>
2023-01-27 13:54:50 +0330

    Display data in table with insertAdjacentHTML.

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const url = "https://jsonplaceholder.typicode.com/users";
 
-fetch(url).then((res) => {
-  res.json().then((json) => {
-    console.log(json);
-  });
-});
+fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json);
+      json.forEach((items) => {
+        const myTable = document.querySelector("#td-data");
+        myTable.insertAdjacentHTML(
+          "beforeend",
+          `
+                            <tr>
+                                <td>${items["id"]}</td>    
+                                <td>${items["name"]}</td>    
+                                <td>${items["username"]}</td>    
+                                <td>${items["email"]}</td>    
+                                <td>${items["phone"]}</td>    
+                                   
+                                      
+    
+                            </tr>
+                        `
+        );
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
